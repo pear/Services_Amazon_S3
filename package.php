514dedb588e6cc8c45f042667306b35bd6edf891
Michael Gauthier <gauthierm@php.net>
2009-11-18 21:57:37 +0000

    Cleanups to file docblock in Services_Amazon_S3 package.xml generator.
    git-svn-id: https://svn.php.net/repository/pear/packages/Services_Amazon_S3/trunk@290949 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -9,23 +9,23 @@
  *
  * LICENSE:
  *
- * Copyright 2008 silverorange
+ * Copyright 2009 silverorange
  *
  * Copyright (c) 2008-2009, Peytz & Co. A/S
- * All rights reserved.
+ * Copyright (c) 2009, silverorange
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the distribution.
- *     * Neither the name of the PHP_LexerGenerator nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the PHP_LexerGenerator nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
